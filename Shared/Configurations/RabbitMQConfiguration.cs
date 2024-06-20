78f2fe09304bcde767dff498d9bd3e4eb55f32d0
Mohaned739 <mohaned.mohamed9999@gmail.com>
2024-06-20 21:24:34 +0300

    Add Shared project which hold common data

---
+++ Shared/Configurations/RabbitMQConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Configurations
+{
+    public class RabbitMQConfiguration
+    {
+        public string Server { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string QueueName { get; set; }
+        public int ConnectionTimeout { get; set; }
+    }
+}
