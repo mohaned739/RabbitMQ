7566e2bd4ccc16f7fe78890c4fbbb2f7a8342a26
Mohaned739 <mohaned.mohamed9999@gmail.com>
2024-06-20 21:25:01 +0300

    add consumer

---
+++ Consumer/DemoConsumer.cs
@@ -0,0 +1,15 @@
+﻿using MassTransit;
+using Shared.Messages;
+
+namespace Consumer
+{
+    internal class DemoConsumer : IConsumer<DemoMessage>
+    {
+        public async Task Consume(ConsumeContext<DemoMessage> context)
+        {
+            var message = context.Message;
+            Console.WriteLine($"From: {message.From}\nTo: {message.To}\nSubject:{message.Title}" +
+                $"\n{message.Content}");
+        }
+    }
+}
---
+++ Consumer/Program.cs
@@ -0,0 +1,46 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Shared.Configurations;
+
+namespace Consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.SetBasePath(AppContext.BaseDirectory)
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                });
+
+            builder.ConfigureServices((hostContext, services) =>
+            {
+                var rabbitMqConfiguration = hostContext.Configuration.GetSection(nameof(RabbitMQConfiguration)).Get<RabbitMQConfiguration>();
+                services.AddMassTransit(x =>
+                {
+                    x.AddConsumer<DemoConsumer>()
+                        .Endpoint(e => e.Name = rabbitMqConfiguration.QueueName);
+                    x.UsingRabbitMq((context, cfg) =>
+                    {
+                        cfg.Host(rabbitMqConfiguration.Server, h =>
+                        {
+                            h.Username(rabbitMqConfiguration.Username);
+                            h.Password(rabbitMqConfiguration.Password);
+                        });
+                        cfg.ConfigureEndpoints(context);
+                        cfg.Exclusive = false;
+                        cfg.ConcurrentMessageLimit = 1;
+
+                    });
+                });
+            });
+
+
+            builder.Build().Run();
+        }
+    }
+}
