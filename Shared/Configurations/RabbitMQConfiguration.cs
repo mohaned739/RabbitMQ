a126cb92a7ba4cb9c5f9d9827a3d4d0629a4ada2
Mohaned739 <mohaned.mohamed9999@gmail.com>
2024-06-20 21:27:03 +0300

    remove timeout config

--- Shared/Configurations/RabbitMQConfiguration.cs
+++ Shared/Configurations/RabbitMQConfiguration.cs
@@ -12,6 +12,5 @@ namespace Shared.Configurations
         public string Username { get; set; }
         public string Password { get; set; }
         public string QueueName { get; set; }
-        public int ConnectionTimeout { get; set; }
     }
 }
